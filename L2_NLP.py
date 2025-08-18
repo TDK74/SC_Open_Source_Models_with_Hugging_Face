8ba200f1fb1a74bac8af626f4aada76184d59804
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-08-18 22:54:38 +0300

    Python file L2_NLP.py added.

---
+++ L2_NLP.py
@@ -0,0 +1,44 @@
+from transformers import pipeline
+from transformers import Conversation
+from transformers.utils import logging
+
+
+logging.set_verbosity_error()
+
+## ------------------------------------------------------##
+chatbot = pipeline(task = "conversational",
+                   model = "./models/facebook/blenderbot-400M-distill")
+
+## ------------------------------------------------------##
+user_message = """
+                What are some fun activities I can do in the winter?
+                """
+
+## ------------------------------------------------------##
+conversation = Conversation(user_message)
+
+## ------------------------------------------------------##
+print(conversation)
+
+## ------------------------------------------------------##
+conversation = chatbot(conversation)
+
+## ------------------------------------------------------##
+print(conversation)
+
+## ------------------------------------------------------##
+conversation.add_message(
+                        {"role" : "user",
+                         "content" : """
+                                    What else do you recommend?
+                                    """
+                        }
+                        )
+
+## ------------------------------------------------------##
+print(conversation)
+
+## ------------------------------------------------------##
+conversation = chatbot(conversation)
+
+print(conversation)
